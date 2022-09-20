17129435a97392856d0847239afa50b7c88394ed
Feriel kacem <ferielkacem92@gmail.com>
2022-09-20 13:48:00 +0200

    adding Firebase

--- src/GeneralCompoments/Feed/Feed.jsx
+++ src/GeneralCompoments/Feed/Feed.jsx
@@ -6,17 +6,9 @@ import Box from "@mui/material/Box";
 import Overall from "../../components/Performance/Overall";
 import Main from "../../components/Insight/Main";
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    marginLeft: "105px",
-    marginRight: "20px",
-  },
-}));
-
 function feed() {
-  const classes = useStyles();
   return (
-    <Box className={classes.container}>
+    <Box style={{ marginLeft: "105px", marginRight: "20px" }}>
       <NavBar />
       <ZoneText />
       <Overall />
